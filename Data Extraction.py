52a7ea60456c26cdb127fc525deef00de2aa9fd9
Kenny <Kenny.barnes20@gmail.com>
2024-06-24 18:38:19 -0400

    Python

---
+++ Data Extraction.py
@@ -0,0 +1,56 @@
+--- Foundation ---
+
+import os
+
+# This is path of my Working Directory
+Project_Directory= "c:\\Users\\kenny\\OneDrive\\Desktop\\--- SCHOOL ---\\LUIT\\Projects\\Project 12\\Gotham\\Bat_Family_DataBase"
+
+# Initialize an empty list to store file information
+file_info_list = []
+
+# Walk through the directory and collect file information
+for root, _, files in os.walk(Project_Directory):
+    for filename in files:
+        file_path = os.path.join(root, filename) 
+        file_size = os.path.getsize(file_path)  # This line will pull the info of the files "file_size"
+        file_info = {'path': file_path, 'size': file_size} # This will combine both, the "file path" and "file size"
+        file_info_list.append(file_info)
+
+# Print the list of dictionaries
+for file_info in file_info_list:
+    print(f"{{'path': '{file_info['path']}', 'size': {file_info['size']}}}")
+
+
+--- Advanced ---
+
+import os
+
+# The start of my function to extract data from the current working directory
+def ADVANCED (Project_Directory=None ):
+
+    # This is the path of my Working Directory
+    Project_Directory= "c:\\Users\\kenny\\OneDrive\\Desktop\\--- SCHOOL ---\\LUIT\\Projects\\Project 12\\Gotham\\Bat_Family_DataBase"
+    
+    if Project_Directory is None:
+        Project_Directory = os.getcwd()
+       
+    # Initialize an empty list to store file information
+    DataBase = []
+
+    # Walk through the directory and collect file information
+    for root, _, files in os.walk(Project_Directory):
+        for filename in files:
+            file_path = os.path.join(root, filename) 
+            file_size = os.path.getsize(file_path)  # This line will pull the info of the files "file_size"
+            Data = {'path': file_path, 'size': file_size} # This will combine both, the "file path" and "file size"
+            DataBase.append(Data)
+    
+    return DataBase
+
+# This is the path of my Working Directory
+Project_Directory= "c:\\Users\\kenny\\OneDrive\\Desktop\\--- SCHOOL ---\\LUIT\\Projects\\Project 12\\Gotham\\Bat_Family_DataBase"
+DataBase = ADVANCED(Project_Directory)
+
+# Print the list of dictionaries
+for Data in DataBase:
+    print(f"{{'path': '{Data['path']}', 'size': {Data['size']}}}")
